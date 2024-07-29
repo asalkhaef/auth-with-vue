14f5d22d935dd542a6103da255b719d6322b7435
asalkhaef <asal.khaef@gmail.com>
2024-07-29 14:29:19 +0330

    Add router

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "../views/LoginView.vue";
+import SignupView from "../views/SignupView.vue";
+
+const routes = [
+  { path: "/login", component: LoginView },
+  { path: "/signup", component: SignupView },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
